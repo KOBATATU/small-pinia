3894710f0bd0b25282f0d2c0c0d66b90b551330f
KOBATATU <kobatatu258@yahoo.co.jp>
2025-04-27 14:41:55 +0900

    add: activePinia

--- packages/small-pinia/createPinia.ts
+++ packages/small-pinia/createPinia.ts
@@ -1,9 +1,10 @@
 import { type App } from "vue";
-import { piniaSymbol, type Pinia } from "./rootStore";
+import { piniaSymbol, setActivePinia, type Pinia } from "./rootStore";
 
 export function createPinia(): Pinia {
   const pinia: Pinia = {
     install(app: App) {
+      setActivePinia(pinia);
       app.provide(piniaSymbol, pinia);
     },
     _s: new Map<string, any>(),
